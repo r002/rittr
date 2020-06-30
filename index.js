db3d5caf6e9247a526cee8c4aea0508cfcd4f6f2
Robert Lin <rcl2106@gmail.com>
2020-06-29 22:30:31 -0400

    r002/rittr#7: Refactor SSE Publisher code into its own module.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
 const { Pool } = require('pg');
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
@@ -9,6 +9,8 @@ const pool = new Pool({
     // for now. Add self-signed cert later. 6/29/20
     // https://stackoverflow.com/questions/54302088/how-to-fix-error-the-server-does-not-support-ssl-connections-when-trying-to-a
 });
+
+var publisher = require('./controllers/publisher');
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -34,47 +36,13 @@ express()
       }
     })
   .get('/times', (req, res) => res.send(showTimes(req)))
-  .get('/events', (req, res) => launchSSE(req, res))
+  .get('/events', (req, res) => publisher.launchSSE(req, res))
   .get('/sub', (req, res) => res.render('pages/sub'))
-  .get('/unsub', (req, res) => unsub(req, res))
+  .get('/unsub', (req, res) => publisher.unsub(req, res))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 showHome = () => {
       return "Landing homepage for rittr!"
-}
-
-var subs = new Set();
-unsub = (req, res) => {
-    var client_id = req.query.id;
-    subs.delete(client_id);
-    let result = `client_id unsubbed: ${client_id}`;
-    console.log(result);
-    res.send(result)
-}
-
-launchSSE = async(req, res) => {
-    // A new client_id has subscribed to our SSE stream.
-    // Keep track of all clients who have subscribed to us.
-    var client_id = req.query.id;
-    subs.add(client_id);
-    let result = `client_id subbed: ${client_id}`;
-    console.log(result);
-
-    // Send the header to prepare the browser for EventSource streaming
-    res.set({
-       'Cache-Control': 'no-cache',
-       'Content-Type': 'text/event-stream',
-       'Connection': 'keep-alive'
-    });
-    res.flushHeaders();
-
-    let count = 0;
-    while (subs.has(client_id))
-    {
-        await new Promise(resolve => setTimeout(resolve, 1000));  // Sleep for one second
-        console.log(`Emit to ${client_id}`, ++count);
-        res.write(`data: ${count}\n\n`);  // Emit an SSE that contains the current 'count' as a string
-    }
 }
 
 showTimes = (req) => {
@@ -86,27 +54,3 @@ showTimes = (req) => {
     console.log(`Return: ${result} | ${req.query.id}`)
     return result;
 }
-
-
-// async launchSSE = (req, res) => {
-//     var client_id = req.query.id;
-//     console.log(`Got /events from ${client_id}`);
-//     res.set({
-//       'Cache-Control': 'no-cache',
-//       'Content-Type': 'text/event-stream',
-//       'Connection': 'keep-alive'
-//     });
-//     res.flushHeaders();
-//
-//     // Tell the client to retry every 10 seconds if connectivity is lost
-//     res.write('retry: 10000\n\n');
-//     let count = 0;
-//
-//     while (true) {
-//       await new Promise(resolve => setTimeout(resolve, 1000));
-//
-//       console.log(`Emit to ${client_id}`, ++count);
-//       // Emit an SSE that contains the current 'count' as a string
-//       res.write(`data: ${count}\n\n`);
-//     }
-// }
